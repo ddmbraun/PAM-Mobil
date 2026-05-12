e19d0919eb81d91b26d7415b3d9590a4a59617a3
ddmbraun <frank.braun@sv-fb.de>
2026-05-12 17:58:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Google-APIs (Drive, Sheets, accounts.google.com) werden NIEMALS gecacht –
 // sie brauchen Auth-Token und müssen immer live abgefragt werden.
 
-const CACHE_NAME = 'pam-mobil-v2';
+const CACHE_NAME = 'pam-mobil-v3';
 const PRECACHE = [
   './',
   './index.html',
