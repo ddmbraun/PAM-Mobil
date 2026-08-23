1b2c41f97bd715cf65c987221a12b84bbf9704a4
ddmbraun <frank.braun@sv-fb.de>
2026-08-23 10:48:31 +0200

    Deploy Mobil

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ﻿// Service Worker - PAM Mobil
 // Google-APIs werden NIEMALS gecacht.
 
-const CACHE_NAME = 'pam-mobil-v232';
+const CACHE_NAME = 'pam-mobil-v233';
 // Aenderungsnotizen stehen bewusst NICHT hier, sondern lokal in CHANGES.md -
 // diese Datei wird oeffentlich ausgeliefert (v183, Datenschutz; wie b646 am Desktop).
 
